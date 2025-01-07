2e817e8ed3eaab047b82da4160ab72b9d36cac52
Mathias Jave Diaz <u202212721@upc.edu.pe>
2025-01-07 17:00:56 -0500

    feat(public): added nav-bar content

--- src/main.js
+++ src/main.js
@@ -1,2 +1,33 @@
 import './style.css'
 import './public.css'
+
+const toggleButton = document.getElementById("toggle-button");
+const sidebar = document.querySelector(".sidebar");
+const overlay = document.createElement("div");
+const closeSideBar = document.getElementById("close-sidebar");
+
+overlay.classList.add("overlay");
+document.body.appendChild(overlay);
+
+toggleButton.addEventListener("click", () => {
+  sidebar.style.left = "0";
+  overlay.classList.add("active");
+});
+
+overlay.addEventListener("click", () => {
+  sidebar.style.left = "-100%";
+  overlay.classList.remove("active");
+});
+
+closeSideBar.addEventListener("click", () => {
+  sidebar.style.left = "-100%";
+  overlay.classList.remove("active");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 1024) {
+    sidebar.style.left = "-100%";
+    overlay.classList.remove("active");
+  }
+});
+
